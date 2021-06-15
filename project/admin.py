50690b6e54adc5da579ae2b0cb52302c66b9dca1
Susheel Arukonda <asusheel23b@gmail.com>
2021-06-15 11:23:54 +0530

    testing project models

--- project/admin.py
+++ project/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
+#Register your models here.
 
-# Register your models here.
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('project_name','start_date','end_date')
+
+admin.site.register(Project,ProjectAdmin)
+admin.site.register(Idea)
--- project/models.py
+++ project/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    project_name = models.CharField(max_length=50,)
+    concept = models.TextField(max_length=500)
+    start_date = models.DateField(auto_now_add=True)
+    end_date = models.DateField()
+
+    def __str__(self):
+        return self.project_name
+
+class Idea(models.Model):
+    idea_desc = models.CharField(max_length=500)
+    added_date = models.DateField(auto_now=True)
+    project = models.ForeignKey(Project,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.idea_desc[0:10]
