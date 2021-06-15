4ec15d442fec51f9607c62528a29f027901f2487
Susheel Arukonda <asusheel23b@gmail.com>
2021-06-15 19:48:38 +0530

    completed tasks,subtasks models

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Task,SubTask
+
 
 # Register your models here.
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('task_name','start_date','end_date')
+
+class SubTaskAdmin(admin.ModelAdmin):
+    list_display = ('sub_name','start_time','end_time','task')
+
+admin.site.register(Task,TaskAdmin)
+admin.site.register(SubTask,SubTaskAdmin)
--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    task_name = models.CharField(max_length=100)
+    task_desc = models.TextField(max_length=500)
+    start_date = models.DateField(auto_now_add=True)
+    end_date = models.DateField()
+    
+    def __str__(self):
+        return self.task_name
+
+class SubTask(models.Model):
+    sub_name = models.CharField(max_length=500)
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+    task = models.ForeignKey(Task,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.sub_name
