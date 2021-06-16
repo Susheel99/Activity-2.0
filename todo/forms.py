5e2a6a887a133ab5c721bf7b193418d5edadf95f
Susheel Arukonda <asusheel23b@gmail.com>
2021-06-17 00:40:56 +0530

    edited form in template

--- todo/forms.py
+++ todo/forms.py
@@ -7,6 +7,11 @@ class TaskForm(forms.ModelForm):
         model = Task
         fields = ['task_name','task_desc','end_date']
 
+    def __init__(self,*args, **kwargs):
+        super(TaskForm, self).__init__(*args, **kwargs)
+
+        self.fields['task_name'].widget.attrs['class'] = 'form-control'
+
 
 class SubTaskForm(forms.ModelForm):
     class Meta:
