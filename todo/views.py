8ce78c41ed3b154d1f6b91785f1f873169ca8ea1
Susheel99 <asusheel23b@gmail.com>
2021-07-13 07:22:26 +0000

    migration merges

--- todo/views.py
+++ todo/views.py
@@ -42,7 +42,7 @@ def index(request):
     tasks = Task.objects.filter(user=request.user).order_by('-end_date')
     createfile(request)
     return render(request,'todo/index.html',{'tasks':tasks})
-    
+
 
 def task_detail(request,id):
     form = SubTaskForm(request.POST or None)
@@ -56,7 +56,7 @@ def hide(request,id):
     subtask = SubTask.objects.get(id=id)
     subtask.is_active = False
     subtask.save()
-    
+
     return HttpResponseRedirect(reverse('task_detail', args=(subtask.task.id,)))
 
 def delete_subtask(self,id):
@@ -64,7 +64,7 @@ def delete_subtask(self,id):
     subtask.delete()
 
     return HttpResponseRedirect(reverse('task_detail', args=(subtask.task.id,)))
-    
+
 
 def add(request,id):
     task = Task.objects.get(id=id)
@@ -75,13 +75,13 @@ def add(request,id):
         start_time = form.cleaned_data['start_time']
         start_date = form.cleaned_data['start_date']
         end_time = form.cleaned_data['end_time']
-        
+
         subtask = SubTask.objects.create(user=request.user,sub_name=subtask_name,start_date=start_date, start_time=start_time, end_time=end_time,task=task,is_active=True)
         subtask.save()
-    
+
     return HttpResponseRedirect(reverse('task_detail', args=(task.id,)))
-    
-    
+
+
 def add_task(request):
     form = TaskForm(request.POST or None)
 
@@ -92,7 +92,7 @@ def add_task(request):
         end_date = form.cleaned_data['end_date']
         task = Task.objects.create(user=request.user,task_name=task_name,task_desc=task_desc,end_date=end_date)
         task.save()
-       
+
         return redirect('index')
     return render(request,'todo/add_task.html',{'form':form})
 
@@ -112,11 +112,11 @@ def all_tasks(request):
     today=datetime.date.today()  # Returns 2018-01-15
     subtasks = SubTask.objects.filter(user=request.user,start_date=today,is_active=True).order_by('start_time')
     cnt = subtasks.count()
-    
-    
+
+
     return render(request,'todo/all_tasks.html',{'subtasks':subtasks,'cnt':cnt})
 
-    
+
 def pending_tasks(request):
     today=datetime.date.today()
     subtasks = SubTask.objects.filter(user=request.user,start_date__lt=today,is_active=True).order_by('start_time')
@@ -124,7 +124,7 @@ def pending_tasks(request):
     return render(request,'todo/pending_tasks.html',{'subtasks':subtasks,'cnt':cnt})
 
 def subtasks_by_date(request):
-    
+
     subtasks = None
     cnt=0
     if request.method == 'POST':
